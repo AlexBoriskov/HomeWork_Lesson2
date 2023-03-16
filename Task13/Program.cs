cfc431f13d803decca681d50932d4faf37d106c4
Alex <sasha-boriskov@yandex.ru>
2023-03-16 22:38:02 +0300

    Выведение третьей цифры в числе

---
+++ Task13/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write ("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n<100)
+{
+    Console.WriteLine ("Данное число не имеет 3 цифру!");
+}
+else
+{
+    int g = (n/100)%10;
+    Console.WriteLine ("Третья цифра в числе = " + g);
+}
