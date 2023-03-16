3ed73a7ffaa13ec79e36058be712a22e856298ce
Alex <sasha-boriskov@yandex.ru>
2023-03-16 23:04:12 +0300

    Рабочая неделя

---
+++ Task15/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write ("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n>0 && n<6)
+{
+    Console.WriteLine ("Рабочие дни");
+}
+else if (n==6 || n==7) 
+{
+    Console.WriteLine ("Выходные");
+}
+else
+{
+    Console.WriteLine ("Некорректное число");
+}
