701e1ec775e8fcc108357ecd7c128a738160954f
Alex <sasha-boriskov@yandex.ru>
2023-03-16 20:49:41 +0300

    Нахождение второй цифры в трехзначном числе

---
+++ Task10/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write ("Введите 3-х значное число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n>=100 && n<=999)
+{
+    int g = (n/10)%10;
+    Console.Write ("Вторая цифра 3-х значного числа: " + g );
+}
+else
+{
+    Console.WriteLine ("Введеное число не 3-х значное");
+}
